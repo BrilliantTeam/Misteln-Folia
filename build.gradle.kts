ed6df7876d1f19b88769f3327837230a53605a7e
RICE0707 <master.end.9377@gmail.com>
2026-09-17 15:53:22 +0800

    1.21.11 適配

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,18 +14,18 @@ paperweight {
 
         patchFile {
             path = "paper-server/build.gradle.kts"
-            outputFile = file("folia-server/build.gradle.kts")
-            patchFile = file("folia-server/build.gradle.kts.patch")
+            outputFile = file("misteln-folia-server/build.gradle.kts")
+            patchFile = file("misteln-folia-server/build.gradle.kts.patch")
         }
         patchFile {
             path = "paper-api/build.gradle.kts"
-            outputFile = file("folia-api/build.gradle.kts")
-            patchFile = file("folia-api/build.gradle.kts.patch")
+            outputFile = file("misteln-folia-api/build.gradle.kts")
+            patchFile = file("misteln-folia-api/build.gradle.kts.patch")
         }
         patchDir("paperApi") {
             upstreamPath = "paper-api"
             excludes = setOf("build.gradle.kts")
-            patchesDir = file("folia-api/paper-patches")
+            patchesDir = file("misteln-folia-api/paper-patches")
             outputDir = file("paper-api")
         }
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "folia"
+rootProject.name = "Misteln-Folia"
 
-include("folia-api")
-include("folia-server")
+include("misteln-folia-api")
+include("misteln-folia-server")
